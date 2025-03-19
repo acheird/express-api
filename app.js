9f6bbcb7b83801a40bb25980946c35e21ee5b3cc
acheird <acheird@gmail.com>
2025-03-19 17:48:17 +0200

    enable json parsing

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const usersRoutes = require("./routes/users");
 
 const app = express();
+
+app.use(express.json());
+
 app.use("/users", usersRoutes);
 
 module.exports = app;
